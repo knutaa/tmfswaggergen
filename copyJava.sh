178b2d1a2c7de16cfcfbe5bbb7628d05656d255e
Bunti <Emanuel Kirchengast>
2018-02-06 14:55:40 +0100

    copyJava.sh copies the 5 most important Java files to the git directory

---
+++ copyJava.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp ~/codegen/swagger-codegen-2.3.1/modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/NodeJSServerCodegen.java ./java/
+cp ~/codegen/swagger-codegen-2.3.1/modules/swagger-codegen/src/main/java/io/swagger/codegen/DefaultCodegen.java ./java/
+cp ~/codegen/swagger-codegen-2.3.1/modules/swagger-codegen/src/main/java/io/swagger/codegen/CodegenOperation.java ./java/
+cp ~/codegen/swagger-codegen-2.3.1/modules/swagger-codegen/src/test/java/io/swagger/codegen/options/NodeJSServerOptionsProvider.java ./java/
+cp ~/codegen/swagger-codegen-2.3.1/modules/swagger-codegen/src/test/java/io/swagger/codegen/nodejs/NodeJSServerOptionsTest.java ./java/
+
